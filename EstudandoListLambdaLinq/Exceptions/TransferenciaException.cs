ba42c956efcee90fc8d9b32f70475f93cb630392
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2018-09-21 10:02:53 -0300

    CÓDIGO INICIAL

---
+++ EstudandoListLambdaLinq/Exceptions/TransferenciaException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ByteBankImportacaoExportacao.Exceptions
+{
+    public class TransferenciaException : FinanceiroException
+    {
+        public TransferenciaException() : this("Erro ao efetuar a transfeência.")
+        {
+        }
+
+        public TransferenciaException(string msg) : base(msg)
+        {
+        }
+
+        public TransferenciaException(string msg, Exception innerException) : base(msg, innerException)
+        {
+        }
+    }
+}
